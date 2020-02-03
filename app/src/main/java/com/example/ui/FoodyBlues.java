6cd336f81f51f84fa8d82b966cec322906ebaf96
sams6 <sams6@camp.rl.kiev.ua>
2020-02-03 16:37:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ui/FoodyBlues.java
@@ -0,0 +1,21 @@
+package com.example.ui;
+
+public class FoodyBlues {
+    int calories, healthy;
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getHealthy() {
+        return healthy;
+    }
+
+    public void setHealthy(int healthy) {
+        this.healthy = healthy;
+    }
+}
